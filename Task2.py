673f946a056540c83dd7ed4afa36cb569c96bb7c
ShrishailK <shrishailkandi95@gmail.com>
2021-08-18 18:56:05 +0530

    Udacity data structures and algorithms project

---
+++ Task2.py
@@ -0,0 +1,30 @@
+"""
+Read file into texts and calls.
+It's ok if you don't understand how to read files
+"""
+import csv
+import operator
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+
+"""
+TASK 2: Which telephone number spent the longest time on the phone
+during the period? Don't forget that time spent answering a call is
+also time spent on the phone.
+Print a message:
+"<telephone number> spent the longest time, <total time> seconds, on the phone during 
+September 2016.".
+"""
+
+d = {}
+for call in calls:
+    for number in call[:2]:
+        d[number] = d.get(number,0) + int(call[3])
+longest_call = max(d.items(),key= operator.itemgetter(1))
+ 
+print("{} spent the longest time, {} seconds, on the phone during September 2016".format(longest_call[0],longest_call[1]))       
